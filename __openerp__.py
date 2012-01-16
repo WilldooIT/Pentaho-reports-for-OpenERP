ac6ed1dbc9e063b7f8334ccff4bb713da0a99512
Deepak Seshadri <deepak@willowit.com.au>
2012-01-16 15:25:27 +1100

    Initial commit.

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+	"name": "Pentaho reports for OpenERP",
+	"description": "Integrates Pentaho's reporting engine with OpenERP using De Bortoli Wines' extension(s).",
+	"version": "0.1",
+	"author": "WillowIT Pty Ltd",
+	"website": "http://www.willowit.com.au/",
+	"depends": ["base"],
+	"category": "Reporting subsystems",
+	"data": [],
+	"installable": True,
+	"active": False
+}
